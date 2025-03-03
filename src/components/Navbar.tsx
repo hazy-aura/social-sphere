861aa4d4418005547a84355f6af4346619a63538
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2025-03-03 17:45:26 +0530

    auth-added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
+"use client"
 import Link from 'next/link';
 import MobileMenu from './MobileMenu';
 import Image from 'next/image';
+import { ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 
 
 
@@ -13,7 +16,7 @@ function Navbar(){
         </div>
 
         {/* CENTER */}
-        <div className="hidden md:flex w-[50%] text-sm" >
+        <div className="hidden md:flex w-[50%] text-sm items-center justify-between" >
                 {/* LINKS */}
             <div className='flex gap-6 text-gray-600'>
             <Link href='/' className='flex items-center gap-2'> 
@@ -36,11 +39,48 @@ function Navbar(){
             Stories
             </span>
             </Link>
-            </div>    
+            </div>  
+            <div className='hidden xl:flex p-2 bg-slate-100 items-center rounded-xl'>
+            <input type="text" placeholder='Search' className='bg-transparent outline-none' />   
+            <Image src="/search.png" alt='' width={14} height={14}/> 
+            </div>  
          </div>
 
         {/* RIGHT */}
-        <div className=" w-[30%] flex items-center gap-4 xl:gap-8 justify-end"> <MobileMenu /></div>
+
+        <div className=" w-[30%] flex items-center gap-4 xl:gap-8 justify-end"> 
+        <ClerkLoading>
+        <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
+        </ClerkLoading>
+        <ClerkLoaded>
+            <SignedIn>
+                <div className='cursor-pointer'>
+                    <Image src="/people.png" alt='' width={20} height={20} />
+                </div>
+                
+                <div className='cursor-pointer'>
+                <Image src="/messages.png" alt='' width={20} height={20} />
+                </div>
+                
+                <div className='cursor-pointer'>
+                <Image src="/notifications.png" alt='' width={20} height={20} />
+                </div>
+                <UserButton />
+            </SignedIn>
+            
+            <SignedOut>
+                <div className='flex items-center gap-2 text-sm'>
+                <Image src="/login.png" alt='' width={20} height={20} />
+                <Link href="/sign-in">Login/Register</Link>
+                </div>
+            </SignedOut>
+
+        </ClerkLoaded>
+
+        <MobileMenu />
+
+        </div>
+        
         
         
         </div>
