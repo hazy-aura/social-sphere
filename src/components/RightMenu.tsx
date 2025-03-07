85acd4ccd8b85bbf4143dd6fd5bc4b1f18fe785e
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2025-03-08 00:30:44 +0530

    rightmenu

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,11 +1,18 @@
 import Ad from "./Ad";
 import Birthdays from "./Birthdays";
 import FriendReq from "./FriendReq";
+import UserInfoCard from "./UserInfoCard";
+import UserMediaCard from "./UserMediaCard";
 
 function RightMenu({userId}:{userId?:string}){
     return(
         <div className=" flex flex-col gap-6">
             
+            {userId?(<>
+            <UserInfoCard userId={userId} />
+            <UserMediaCard userId={userId} />
+            </>):null}
+
             <FriendReq />
             <Birthdays />
             <Ad size="md"/>
--- src/components/UserMediaCard.tsx
+++ src/components/UserMediaCard.tsx
@@ -1,11 +1,81 @@
+import Image from "next/image";
+import Link from "next/link";
 
-
-const UserMediaCard = () => {
-    return (
-        <div className="user-info-card">
-         
+const UserMediaCard = ({ userId }: { userId: string }) => {
+  return (
+    <>
+      <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+        {/* TOP */}
+        <div className="flex justify-between items-center font-medium">
+          <span className="text-gray-500"> User Media</span>
+          <Link href="/" className="text-blue-500 text-xs">
+            See all
+          </Link>
         </div>
-    );
+        {/* BOTTOM */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <div className="relative w-1/5 h-24">
+            <Image
+              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+          <div className="relative w-1/5 h-24">
+            <Image
+              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              className="object-cover rounded-md"
+            />
+          </div><div className="relative w-1/5 h-24">
+            <Image
+              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              className="object-cover rounded-md"
+            />
+          </div><div className="relative w-1/5 h-24">
+            <Image
+              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              className="object-cover rounded-md"
+            />
+          </div><div className="relative w-1/5 h-24">
+            <Image
+              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              className="object-cover rounded-md"
+            />
+          </div><div className="relative w-1/5 h-24">
+            <Image
+              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              className="object-cover rounded-md"
+            />
+          </div><div className="relative w-1/5 h-24">
+            <Image
+              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              className="object-cover rounded-md"
+            />
+          </div><div className="relative w-1/5 h-24">
+            <Image
+              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default UserMediaCard; 
+export default UserMediaCard;
