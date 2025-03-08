ad5323bf28d15fcce174299ffcb75646df90de64
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2025-03-09 00:37:23 +0530

    home-page added

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import React from "react";
+
+const ProfileCard = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
+      <div className="h-20 relative">
+        <Image
+          src="https://images.pexels.com/photos/20302214/pexels-photo-20302214/free-photo-of-red-tree-leaves-over-table-and-stone-seats.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+          fill
+          className="object-cover rounded-md "
+        />
+        <Image
+          src="https://images.pexels.com/photos/8521518/pexels-photo-8521518.png?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+          width={48}
+          height={48}
+          className="object-cover rounded-full w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10"
+        />
+      </div>
+      <div className="flex flex-col gap-2 items-center h-20">
+        <span className="font-semibold">Marshal Mathers</span>
+        <div className="flex-items-center gap-4">
+          <div className="flex">
+            <Image
+              src="https://images.pexels.com/photos/8521518/pexels-photo-8521518.png?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              width={12}
+              height={12}
+              className="object-cover rounded-full w-3 h-3 "
+            />
+            <Image
+              src="https://images.pexels.com/photos/8521518/pexels-photo-8521518.png?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              width={12}
+              height={12}
+              className="object-cover rounded-full w-3 h-3 "
+            />
+
+            <Image
+              src="https://images.pexels.com/photos/8521518/pexels-photo-8521518.png?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              width={12}
+              height={12}
+              className="object-cover rounded-full w-3 h-3 "
+            />
+          </div>
+          <span className="text-xs text-gray-500"></span>
+        </div>
+        <button className="bg-blue-500 text-white text-xs p-2 rounded-md">My Profile</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
