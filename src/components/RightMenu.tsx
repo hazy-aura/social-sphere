d2813c186e23afa8797ed2b212eb45aecc1bbbbd
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2025-03-16 16:15:11 +0530

    added suspense

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -4,21 +4,27 @@ import Birthdays from "./Birthdays";
 import FriendReq from "./FriendReq";
 import UserInfoCard from "./UserInfoCard";
 import UserMediaCard from "./UserMediaCard";
+import { Suspense } from "react";
 
-function RightMenu({user}:{user?:User}){
-    return(
-        <div className=" flex flex-col gap-6">
-            
-            {user?(<>
+function RightMenu({ user }: { user?: User }) {
+  return (
+    <div className=" flex flex-col gap-6">
+      {user ? (
+        <>
+          <Suspense fallback="loading...">
             <UserInfoCard user={user} />
+          </Suspense>
+          <Suspense fallback="loading...">
             <UserMediaCard user={user} />
-            </>):null}
+          </Suspense>
+        </>
+      ) : null}
 
-            <FriendReq />
-            <Birthdays />
-            <Ad size="md"/>
-        </div>
-    )
+      <FriendReq />
+      <Birthdays />
+      <Ad size="md" />
+    </div>
+  );
 }
 
 export default RightMenu;
--- src/components/UserInfoCard.tsx
+++ src/components/UserInfoCard.tsx
@@ -1,17 +1,53 @@
+import prisma from "@/lib/Client";
+import { auth } from "@clerk/nextjs/server";
 import { User } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-const UserInfoCard = ({ user }: { user: User }) => {
-
-  const createdAtDate =new Date(user.createdAt);
+const UserInfoCard = async ({ user }: { user: User }) => {
+  const createdAtDate = new Date(user.createdAt);
 
   const formattedDate = createdAtDate.toLocaleDateString("en-US", {
     month: "short",
     day: "numeric",
     year: "numeric",
   });
-   
+
+  let isUserBlocked = false;
+  let isFollowing = false;
+  let isFollowingRequestSent = false;
+
+  const { userId: currentUserId } = auth();
+
+  if (currentUserId) {
+    const blockRes = await prisma.block.findFirst({
+      where: {
+        blockerId: currentUserId,
+        blockedId: user.id,
+      },
+    });
+
+    blockRes ? (isUserBlocked = true) : (isUserBlocked = false);
+
+    const followRes = await prisma.follower.findFirst({
+      where: {
+        followerId: currentUserId,
+        followingId: user.id,
+      },
+    });
+    followRes ? (isFollowing = true) : (isFollowing = false);
+
+    const followReqRes = await prisma.followRequest.findFirst({
+      where: {
+        senderId: currentUserId,
+        receiverId: user.id,
+      },
+    });
+    followReqRes
+      ? (isFollowingRequestSent = true)
+      : (isFollowingRequestSent = false);
+  }
+
   return (
     <>
       <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
@@ -62,13 +98,15 @@ const UserInfoCard = ({ user }: { user: User }) => {
           )}
 
           <div className="flex items-center justify-between">
-            {user.website && <div className="flex gap-1 items-center">
-              <Image src="/link.png" alt="" width={16} height={16} />
-              <Link className="text-blue-500 font-medium" href={user.website}>
-                {user.website}
-              </Link>
-            </div>}
-            
+            {user.website && (
+              <div className="flex gap-1 items-center">
+                <Image src="/link.png" alt="" width={16} height={16} />
+                <Link className="text-blue-500 font-medium" href={user.website}>
+                  {user.website}
+                </Link>
+              </div>
+            )}
+
             <div className="flex gap-1 items-center">
               <Image src="/date.png" alt="" width={16} height={16} />
               <span>Joined {formattedDate} </span>
