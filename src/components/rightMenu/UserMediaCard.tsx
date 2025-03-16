3ba0c154c88d7f2dbc19218e9e58f1a05aee7d14
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2025-03-17 00:43:20 +0530

    added dynamic posts

--- src/components/rightMenu/UserMediaCard.tsx
+++ src/components/rightMenu/UserMediaCard.tsx
@@ -1,8 +1,21 @@
+import prisma from "@/lib/Client";
 import { User } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-const UserMediaCard = ({ user }: { user: User }) => {
+const UserMediaCard = async ({ user }: { user: User }) => {
+  const postsWithMedia = await prisma.post.findMany({
+    where: {
+      userId: user.id,
+      img: {
+        not: null,
+      },
+    },
+    take: 8,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
     <>
       <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
@@ -15,64 +28,15 @@ const UserMediaCard = ({ user }: { user: User }) => {
         </div>
         {/* BOTTOM */}
         <div className="flex gap-4 justify-between flex-wrap">
-          <div className="relative w-1/5 h-24">
+          {postsWithMedia.length? postsWithMedia.map(post=> <div className="relative w-1/5 h-24" key={post.id}>
             <Image
-              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              src={post.img!}
               alt=""
               fill
               className="object-cover rounded-md"
             />
-          </div>
-          <div className="relative w-1/5 h-24">
-            <Image
-              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              className="object-cover rounded-md"
-            />
-          </div><div className="relative w-1/5 h-24">
-            <Image
-              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              className="object-cover rounded-md"
-            />
-          </div><div className="relative w-1/5 h-24">
-            <Image
-              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              className="object-cover rounded-md"
-            />
-          </div><div className="relative w-1/5 h-24">
-            <Image
-              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              className="object-cover rounded-md"
-            />
-          </div><div className="relative w-1/5 h-24">
-            <Image
-              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              className="object-cover rounded-md"
-            />
-          </div><div className="relative w-1/5 h-24">
-            <Image
-              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              className="object-cover rounded-md"
-            />
-          </div><div className="relative w-1/5 h-24">
-            <Image
-              src="https://images.pexels.com/photos/30872674/pexels-photo-30872674/free-photo-of-dramatic-cliffs-and-coastline-of-madeira-portugal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              className="object-cover rounded-md"
-            />
-          </div>
+          </div>): <p className="text-gray-500">No media found</p>}
+
         </div>
       </div>
     </>
