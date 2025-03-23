22ddd5e6aaa19202ed85441c56126f3b09223367
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2025-03-24 02:10:06 +0530

    added form and validations

--- src/components/rightMenu/UpdateUser.tsx
+++ src/components/rightMenu/UpdateUser.tsx
@@ -1,12 +1,162 @@
-import React from 'react';
+"use client";
 
-const UpdateUser = () => {
-    return (
-        <div>
-            <h1>Update User</h1>
-            {/* Add your form or update logic here */}
-        </div>
-    );
+import { updateProfile } from "@/lib/actions";
+import { User } from "@prisma/client";
+import Image from "next/image";
+import { useState } from "react";
+
+const UpdateUser = ({ user }: { user: User }) => {
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+  return (
+    <>
+      <div className="">
+        <span
+          className=" text-blue-500 cursor-pointer text-xs"
+          onClick={() => setOpen(true)}
+        >
+          Update
+        </span>
+        {open && (
+          <div className="absolute w-screen h-screen top-0 left-0 bg-black bg-opacity-65 flex justify-center items-center z-50">
+            <form
+              action={updateProfile}
+              className="p-12 bg-white rounded-lg shadow-md flex flex-col gap-2 w-full md:w-1/2 xl:w-1/3 relative"
+            >
+              {/* TITLE */}
+              <h1>Update Profile</h1>
+              <div className="mt-4 text-xs text-gray-500">
+                Use the navbar profile to change the avatar or username.
+              </div>
+              {/* COVER PIC UPLOAD */}
+
+              <div className="flex flex-col gap-4 my-4">
+                <label htmlFor=""> Cover Picture</label>
+                <div className="flex items-center gap-2 cursor-pointer">
+                  <Image
+                    src={user.cover || "/noCover.png"}
+                    width={48}
+                    height={32}
+                    alt=""
+                    className="w-12 h-8 rounded-md object-cover"
+                  />
+                  <span className="text-xs underline text-gray-600">
+                    {" "}
+                    Change
+                  </span>
+                </div>
+              </div>
+
+              {/* WRAPPER */}
+              <div className="flex flex-wrap justify-between gap-2 xl:gap-4">
+                {/* INPUT */}
+                <div className="flex flex-col gap-4">
+                  <label htmlFor="" className="text-xs text-gray-500">
+                    {" "}
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    placeholder={user.name || "John"}
+                    className="ring-1 ring-gray-300  p-[13px] rounded-md text-sm"
+                    name="name"
+                  />
+                </div>
+                {/* INPUT */}
+                <div className="flex flex-col gap-4">
+                  <label htmlFor="" className="text-xs text-gray-500">
+                    {" "}
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    placeholder={user.surname || "Doe"}
+                    className="ring-1 ring-gray-300  p-[13px] rounded-md text-sm"
+                    name="surname"
+                  />
+                </div>
+                {/* INPUT */}
+                <div className="flex flex-col gap-4">
+                  <label htmlFor="" className="text-xs text-gray-500">
+                    {" "}
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    placeholder={user.description || "Life is Unfair.."}
+                    className="ring-1 ring-gray-300  p-[13px] rounded-md text-sm"
+                    name="description"
+                  />
+                </div>
+                {/* INPUT */}
+                <div className="flex flex-col gap-4">
+                  <label htmlFor="" className="text-xs text-gray-500">
+                    {" "}
+                    City
+                  </label>
+                  <input
+                    type="text"
+                    placeholder={user.city || "Dehradun"}
+                    className="ring-1 ring-gray-300  p-[13px] rounded-md text-sm"
+                    name="city"
+                  />
+                </div>
+                {/* INPUT */}
+                <div className="flex flex-col gap-4">
+                  <label htmlFor="" className="text-xs text-gray-500">
+                    {" "}
+                    School
+                  </label>
+                  <input
+                    type="text"
+                    placeholder={user.school || "CST"}
+                    className="ring-1 ring-gray-300  p-[13px] rounded-md text-sm"
+                    name="school"
+                  />
+                </div>
+                {/* INPUT */}
+                <div className="flex flex-col gap-4">
+                  <label htmlFor="" className="text-xs text-gray-500">
+                    {" "}
+                    Work
+                  </label>
+                  <input
+                    type="text"
+                    placeholder={user.work || "Wipro"}
+                    className="ring-1 ring-gray-300  p-[13px] rounded-md text-sm"
+                    name="work"
+                  />
+                </div>
+                {/* INPUT */}
+                <div className="flex flex-col gap-4">
+                  <label htmlFor="" className="text-xs text-gray-500">
+                    {" "}
+                    Website
+                  </label>
+                  <input
+                    type="text"
+                    placeholder={user.website || "www.goooogle.com"}
+                    className="ring-1 ring-gray-300  p-[13px] rounded-md text-sm"
+                    name="website"
+                  />
+                </div>
+              </div>
+              <button className="bg-blue-500 text-white p-2 mt-2 rounded-md">
+                <span>Update</span>
+              </button>
+
+              <div
+                className="absolute text-xl right-2 top-3 cursor-pointer"
+                onClick={handleClose}
+              >
+                X
+              </div>
+            </form>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default UpdateUser;
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,158 +2,187 @@
 
 import { auth } from "@clerk/nextjs/server";
 import prisma from "./Client";
+import { z } from "zod";
 
 export const switchFollow = async (userId: string) => {
-    const authData = auth();
-    const currentUserId = authData?.userId;
+  const authData = auth();
+  const currentUserId = authData?.userId;
 
-    if (!currentUserId) {
-        throw new Error("User not authenticated");
+  if (!currentUserId) {
+    throw new Error("User not authenticated");
+  }
+
+  try {
+    // Check if the user is already following
+    const existingFollow = await prisma.follower.findFirst({
+      where: {
+        followerId: currentUserId,
+        followingId: userId,
+      },
+    });
+
+    if (existingFollow) {
+      await prisma.follower.delete({
+        where: { id: existingFollow.id },
+      });
+      return { status: "Unfollowed" };
     }
 
-    try {
-        // Check if the user is already following
-        const existingFollow = await prisma.follower.findFirst({
-            where: {
-                followerId: currentUserId,
-                followingId: userId,
-            },
-        });
+    // Check if a follow request exists
+    const existingFollowReq = await prisma.followRequest.findFirst({
+      where: {
+        senderId: currentUserId,
+        receiverId: userId,
+      },
+    });
 
-        if (existingFollow) {
-            await prisma.follower.delete({
-                where: { id: existingFollow.id },
-            });
-            return { status: "Unfollowed" };
-        }
-
-        // Check if a follow request exists
-        const existingFollowReq = await prisma.followRequest.findFirst({
-            where: {
-                senderId: currentUserId,
-                receiverId: userId,
-            },
-        });
-
-        if (existingFollowReq) {
-            await prisma.followRequest.delete({
-                where: { id: existingFollowReq.id },
-            });
-            return { status: "Follow Request Cancelled" };
-        }
-
-        // Create a new follow request
-        await prisma.followRequest.create({
-            data: {
-                senderId: currentUserId,
-                receiverId: userId,
-            },
-        });
-
-        return { status: "Follow Request Sent" };
-
-    } catch (err) {
-        console.error("Error in switchFollow:", err);
-        throw new Error("Something went wrong");
+    if (existingFollowReq) {
+      await prisma.followRequest.delete({
+        where: { id: existingFollowReq.id },
+      });
+      return { status: "Follow Request Cancelled" };
     }
+
+    // Create a new follow request
+    await prisma.followRequest.create({
+      data: {
+        senderId: currentUserId,
+        receiverId: userId,
+      },
+    });
+
+    return { status: "Follow Request Sent" };
+  } catch (err) {
+    console.error("Error in switchFollow:", err);
+    throw new Error("Something went wrong");
+  }
 };
 
 export const switchBlock = async (userId: string) => {
+  const { userId: currentUserId } = auth();
 
-    const {userId: currentUserId} = auth();
+  if (!currentUserId) {
+    throw new Error("User not authenticated");
+  }
 
-    if(!currentUserId){
-        throw new Error("User not authenticated");
-    }
-
-    try{
-        const existingBlock = await prisma.block.findFirst({
-            where:{
-                blockerId: currentUserId,
-                blockedId: userId,
-            }
-        });
-        if(existingBlock){
-            await prisma.block.delete({
-                where: {id: existingBlock.id},
-            });
-            return {status: "Unblocked"};
-        }else{
-            await prisma.block.create({
-                data: {
-                    blockerId: currentUserId,
-                    blockedId: userId,
-                },
-            });
-            return {status: "Blocked"};
-        }
-    }
-    catch(err){
-        console.log(err);
-        throw new Error("Something went wrong");
-    }
-};
-
-
-export const acceptFollowRequest = async (userId: string) => {
-    const {userId: currentUserId} = auth();
-
-    if (!currentUserId) {
-        throw new Error("User not authenticated");
-    }
-
-    try{
-    const existingFollowReq = await prisma.followRequest.findFirst({
-        where: {
-            senderId: userId,
-            receiverId: currentUserId,
-        },
+  try {
+    const existingBlock = await prisma.block.findFirst({
+      where: {
+        blockerId: currentUserId,
+        blockedId: userId,
+      },
     });
-    
-    if(existingFollowReq){
-        await prisma.followRequest.delete({
-            where: {id: existingFollowReq.id},
-        });
-        
-        await prisma.follower.create({
-            data: {
-                followerId: currentUserId,
-                followingId: userId,
-            },
-        });
-
-    }
-    } catch (err) {
-        console.log(err);
-        throw new Error("Something went wrong");
-    }
-}
-
-
-export const declineFollowRequest = async (userId: string) => {
-    const {userId: currentUserId} = auth();
-
-    if (!currentUserId) {
-        throw new Error("User not authenticated");
-    }
-
-    try{
-    const existingFollowReq = await prisma.followRequest.findFirst({
-        where: {
-            senderId: userId,
-            receiverId: currentUserId,
+    if (existingBlock) {
+      await prisma.block.delete({
+        where: { id: existingBlock.id },
+      });
+      return { status: "Unblocked" };
+    } else {
+      await prisma.block.create({
+        data: {
+          blockerId: currentUserId,
+          blockedId: userId,
         },
-    });
-    
-    if(existingFollowReq){
-        await prisma.followRequest.delete({
-            where: {id: existingFollowReq.id},
-        });
-        
+      });
+      return { status: "Blocked" };
     }
-}catch(err){
+  } catch (err) {
     console.log(err);
     throw new Error("Something went wrong");
-}
-    
-}
+  }
+};
+
+export const acceptFollowRequest = async (userId: string) => {
+  const { userId: currentUserId } = auth();
+
+  if (!currentUserId) {
+    throw new Error("User not authenticated");
+  }
+
+  try {
+    const existingFollowReq = await prisma.followRequest.findFirst({
+      where: {
+        senderId: userId,
+        receiverId: currentUserId,
+      },
+    });
+
+    if (existingFollowReq) {
+      await prisma.followRequest.delete({
+        where: { id: existingFollowReq.id },
+      });
+
+      await prisma.follower.create({
+        data: {
+          followerId: currentUserId,
+          followingId: userId,
+        },
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    throw new Error("Something went wrong");
+  }
+};
+
+export const declineFollowRequest = async (userId: string) => {
+  const { userId: currentUserId } = auth();
+
+  if (!currentUserId) {
+    throw new Error("User not authenticated");
+  }
+
+  try {
+    const existingFollowReq = await prisma.followRequest.findFirst({
+      where: {
+        senderId: userId,
+        receiverId: currentUserId,
+      },
+    });
+
+    if (existingFollowReq) {
+      await prisma.followRequest.delete({
+        where: { id: existingFollowReq.id },
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    throw new Error("Something went wrong");
+  }
+};
+
+export const updateProfile = async (formData: FormData) => {
+  const fields = Object.fromEntries(formData);
+  console.log(fields);
+
+  const Profile = z.object({
+    cover: z.string().optional(),
+    name: z.string().max(60).optional(),
+    surname: z.string().max(60).optional(),
+    description: z.string().max(300).optional(),
+    city: z.string().max(60).optional(),
+    school: z.string().max(60).optional(),
+    work: z.string().max(60).optional(),
+    website: z.string().max(60).optional(),
+  });
+
+  const validatedProfile = Profile.safeParse(fields);
+
+  if (!validatedProfile.success) {
+    console.log(validatedProfile.error.flatten().fieldErrors);
+    return "error";
+  }
+  const { userId } = auth();
+  if (!userId) return "user not authenticated";
+
+  try {
+    await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: validatedProfile.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
