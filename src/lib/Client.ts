36952de62e13fae1e61d658b5e59194326155164
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2025-04-27 02:37:27 +0530

    prismaclient err fix

--- src/lib/Client.ts
+++ src/lib/Client.ts
@@ -1,4 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+
+// Use a global variable to prevent exhausting connections in development
+declare global {
+  var __prisma: PrismaClient | undefined;
+}
+
+const prisma = global.__prisma ?? new PrismaClient();
+if (process.env.NODE_ENV === "development") global.__prisma = prisma;
 
 export default prisma;
